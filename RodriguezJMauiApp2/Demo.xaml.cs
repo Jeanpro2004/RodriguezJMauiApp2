bf3b35661ecaa423d2bb060a7c09101c3ee38520
Jeanpro2004 <jean.rodriguez.ortiz@udla.edu.ec>
2025-05-29 08:49:37 -0500

    Implements Repositry and Functions

--- RodriguezJMauiApp2/Demo.xaml.cs
+++ RodriguezJMauiApp2/Demo.xaml.cs
@@ -1,9 +1,37 @@
+using RodriguezJMauiApp2.Repositorios;
+using System.Threading.Tasks;
+
 namespace RodriguezJMauiApp2;
 
 public partial class Demo : ContentPage
 {
+	private ManejoArchivosRepository _repo;
 	public Demo()
 	{
+		_repo = new ManejoArchivosRepository();
+
 		InitializeComponent();
+        ObtenerInformacionArchivo();
+        
 	}
+
+	private async Task ObtenerInformacionArchivo()
+	{
+		texto1.Text = await _repo.ObtenerInformacionArchivo();
+    }
+
+	private async void BotonGuardar_Clicked(object sender , EventArgs e)
+	{
+		string texto = texto1.Text;
+        Boolean guardar = await _repo.GuardarArchivo(texto);
+		TextoArchivo.Text = await _repo.ObtenerInformacionArchivo();
+    
+        if (!guardar)
+		{
+            throw new Exception("No se pudo guardar el archivo");
+        }
+
+
+    }
+
 }
---
+++ RodriguezJMauiApp2/Repositorios/ManejoArchivosRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RodriguezJMauiApp2.Repositorios
+{
+    class ManejoArchivosRepository
+    {
+        private string path = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+        public async Task<bool> GuardarArchivo(string texto)
+        {
+            try
+            {
+               
+                await File.WriteAllTextAsync(path, texto);
+
+                if (File.Exists(path))
+                {
+                    return true;
+
+                }
+                    return false;
+
+                }
+
+             catch (Exception)
+            {
+                return false;
+            }
+
+        }
+         
+        public async Task<string> ObtenerInformacionArchivo()
+            {
+            if (File.Exists(path))
+            {
+                string texto = await File.ReadAllTextAsync(path);
+                return texto;
+            }
+            return "No enccontre nada en el archivo";
+        }
+
+        
+    }
+}
